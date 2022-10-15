c70ad8249c58a8cadea2b7f22a9d0a4362872ffd
Lee Pang <leepang15@gmail.com>
2022-10-15 02:29:28 -0500

    Load timestamp, name into dataframe

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import json
 import re
+import pandas as pd
 
 def load_json():
     with open('json/message_1.json') as file:
         data = json.load(file)
         
-    count = 0
-    x = 0
+    df = pd.DataFrame()
     for message in data['messages']:
-        if 'content' in message:
-            x += 1
-            cont = message['content']
-            if not re.search('^.*reacted.*to your message $', cont):
-                count += 1
-    print(count)
-    print(x)
+        if 'content' in message and re.search('^.*reacted.*to your message $', message['content']):
+            pass
+        else:
+            new_row = {'timestamp':message['timestamp_ms'], 'name':message['sender_name']}
+            df = pd.concat([df, pd.DataFrame([new_row])])
+    print(df)
+    
+    
 
 if __name__ == '__main__':
     load_json()

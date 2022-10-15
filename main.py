45b50aae1b7d3244d5630f40b15206a84946e67a
Lee Pang <leepang15@gmail.com>
2022-10-15 02:59:10 -0500

    Add message type to dataframe

--- main.py
+++ main.py
@@ -11,11 +11,25 @@ def load_json():
         if 'content' in message and re.search('^.*reacted.*to your message $', message['content']):
             pass
         else:
-            new_row = {'timestamp':message['timestamp_ms'], 'name':message['sender_name']}
+            if message['is_unsent']:
+                message_type = 'unsent'
+            if 'content' in message:
+                message_type = 'content'
+            elif 'gifs' in message:
+                message_type = 'gifs'
+            elif 'photos' in message:
+                message_type = 'photos'
+            elif 'audio_files' in message:
+                message_type = 'audio_files'
+            elif 'videos' in message:
+                message_type = 'videos'
+            elif 'sticker' in message:
+                message_type = 'sticker'
+            new_row = {'timestamp':message['timestamp_ms'], 'name':message['sender_name'], 'message_type':message_type}
             df = pd.concat([df, pd.DataFrame([new_row])])
     print(df)
-    
-    
+    print(df.groupby(['message_type']).count())
+
 
 if __name__ == '__main__':
     load_json()

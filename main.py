f1aa62c8492af23771f0209fcdeab8ceb5483eb9
Lee Pang <leepang15@gmail.com>
2022-10-19 17:06:01 -0500

    Read multiple json files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import re
 import pandas as pd
@@ -16,37 +17,38 @@ def filter_stats(df, current_date = date.today(), begin = None, end = -1):
     return by_date.groupby(['name'])['count'].count()
 
 def load_json():
-    with open('json/message_1.json') as file:
-        data = json.load(file)
-    
     message_list = []
-    for message in data['messages']:
-        if 'content' in message and re.search('^.*reacted.*to your message $', message['content']):
-            pass
-        else:
-            if message['is_unsent']:
-                message_type = 'unsent'
-                count = 1
-            if 'content' in message:
-                message_type = 'content'
-                count = len(message['content'].split())
-            elif 'gifs' in message:
-                message_type = 'gifs'
-                count = len(message['gifs'])
-            elif 'photos' in message:
-                message_type = 'photos'
-                count = len(message['photos'])
-            elif 'audio_files' in message:
-                message_type = 'audio_files'
-                count = len(message['audio_files'])
-            elif 'videos' in message:
-                message_type = 'videos'
-                count = len(message['videos'])
-            elif 'sticker' in message:
-                message_type = 'sticker'
-                count = 1
-            new_row = [message['timestamp_ms'], message['sender_name'], message_type, count]
-            message_list.append(new_row)
+    for filename in os.listdir('json'):
+        with open(os.path.join('json', filename)) as file:
+            data = json.load(file)
+    
+        for message in data['messages']:
+            if 'content' in message and re.search('^.*reacted.*to your message $', message['content']):
+                pass
+            else:
+                if message['is_unsent']:
+                    message_type = 'unsent'
+                    count = 1
+                if 'content' in message:
+                    message_type = 'content'
+                    count = len(message['content'].split())
+                elif 'gifs' in message:
+                    message_type = 'gifs'
+                    count = len(message['gifs'])
+                elif 'photos' in message:
+                    message_type = 'photos'
+                    count = len(message['photos'])
+                elif 'audio_files' in message:
+                    message_type = 'audio_files'
+                    count = len(message['audio_files'])
+                elif 'videos' in message:
+                    message_type = 'videos'
+                    count = len(message['videos'])
+                elif 'sticker' in message:
+                    message_type = 'sticker'
+                    count = 1
+                new_row = [message['timestamp_ms'], message['sender_name'], message_type, count]
+                message_list.append(new_row)
             
     df = pd.DataFrame(message_list, columns=['timestamp_ms', 'name', 'message_type', 'count'])
     df['date'] = df['timestamp_ms'].map(truncate_timestamp)

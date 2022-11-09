36fdb909fd550a881deeb816b360840356156f4f
Lee Pang <leepang15@gmail.com>
2022-11-08 17:15:51 -0800

    Print total message counts by day

---
+++ daily_total_counts.py
@@ -0,0 +1,53 @@
+import os
+import json
+import re
+import pandas as pd
+from datetime import datetime, date, timedelta
+
+def truncate_timestamp(timestamp_ms):
+    dt = datetime.fromtimestamp(timestamp_ms/1000)
+    return date(dt.year, dt.month, dt.day)
+
+def load_json():
+    message_list = []
+    for filename in os.listdir('json'):
+#    for filename in ['message_1026_1029.json']:    
+        with open(os.path.join('json', filename)) as file:
+            data = json.load(file)
+    
+        for message in data['messages']:
+            if 'content' in message and re.search('^.*reacted.*to your message $', message['content']):
+                pass
+            else:
+                if message['is_unsent']:
+                    message_type = 'unsent'
+                    count = 1
+                if 'content' in message:
+                    message_type = 'content'
+                    count = len(message['content'].split())
+                elif 'gifs' in message:
+                    message_type = 'gifs'
+                    count = len(message['gifs'])
+                elif 'photos' in message:
+                    message_type = 'photos'
+                    count = len(message['photos'])
+                elif 'audio_files' in message:
+                    message_type = 'audio_files'
+                    count = len(message['audio_files'])
+                elif 'videos' in message:
+                    message_type = 'videos'
+                    count = len(message['videos'])
+                elif 'sticker' in message:
+                    message_type = 'sticker'
+                    count = 1
+                new_row = [message['timestamp_ms'], message['sender_name'], message_type, count]
+                message_list.append(new_row)
+            
+    df = pd.DataFrame(message_list, columns=['timestamp_ms', 'name', 'message_type', 'count'])
+    df['date'] = df['timestamp_ms'].map(truncate_timestamp)
+    return df[df['message_type'] != 'unsent']
+
+if __name__ == '__main__':
+    sent_messages = load_json()
+    counts = sent_messages.groupby(['date'])['count'].count()
+    print(counts)

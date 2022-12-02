5ffc2403929800463a164f7a833a83f20ee12fe1
Lee Pang <leepang15@gmail.com>
2022-12-01 20:39:50 -0600

    Update nicknames file

--- update_nicknames.py
+++ update_nicknames.py
@@ -1,38 +1,33 @@
+import os
 import pandas as pd
-import csv
-
-def update_nicknames():
-    old = pd.read_csv('nicknames.csv', names=['name', 'nickname1'])
-    new = pd.read_csv('new_nicknames.csv', names=['name', 'nickname2'])
-    df = old.merge(new, left_on='name', right_on='name', how='outer')
-    #print(df.to_string())
-    change = df[df['nickname1'] != df['nickname2']]
-    print(change)
-    
-    old2 = pd.read_csv('nicknames.csv', names=['name', 'nickname'])
-    new2 = pd.read_csv('new_nicknames.csv', names=['name', 'nickname'])
-    
-    old_set = set(old2['name'])
-    new_set = set(new2['name'])
-    not_in_new = old_set - new_set
-    print(not_in_new)
-    old3 = old2[old2['name'].isin(not_in_new)]
-    updated = pd.concat([new2, old3])
-    print(updated.to_string())
-    
-    updated.to_csv('updated_nicknames.csv', header=False, index=False)
-    
-    with open('updated_nicknames.csv', mode='r') as infile:
-        reader = csv.reader(infile)
-        names_dict = {rows[0]:rows[1] for rows in reader}
-    print(len(names_dict))
-    
-    #df2 = old2.combine_first(new2)
-    #print(df2)
-    #df3 = new2.combine_first(old2)
-    #print(df3.to_string())
-    #old2.update(new2)
-    #print(old2.to_string())
+#import csv
 
 if __name__ == '__main__':
-    update_nicknames()
+#def update_nicknames():
+    old_file = os.path.join('csv', 'old_nicknames.csv')
+    new_file = os.path.join('csv', 'new_nicknames.csv')
+    old = pd.read_csv(old_file, names=['name', 'nickname'])
+    new = pd.read_csv(new_file, names=['name', 'nickname'])
+    
+#    old2 = pd.read_csv('nicknames.csv', names=['name', 'nickname'])
+#    new2 = pd.read_csv('new_nicknames.csv', names=['name', 'nickname'])
+    
+#    old_names = set(old['name'])
+#    new_names = set(new['name'])
+    not_in_new = set(old['name']) - set(new['name'])
+#    print(not_in_new)
+    old2 = old[old['name'].isin(not_in_new)]
+    updated = pd.concat([new, old2])
+#    print(updated.to_string())
+    
+    updated_file = os.path.join('csv', 'updated_nicknames.csv')
+    updated.to_csv(updated_file, header=False, index=False)
+    
+#    with open('updated_nicknames.csv', mode='r') as infile:
+#        reader = csv.reader(infile)
+#        names_dict = {rows[0]:rows[1] for rows in reader}
+#    print(len(names_dict))
+
+
+
+#    update_nicknames()

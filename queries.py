4a3c08f0e3e85e5202057f511e7083f71fc0a062
Lee Pang <leepang15@gmail.com>
2022-12-23 00:12:22 -0600

    Rename db

--- queries.py
+++ queries.py
@@ -2,7 +2,7 @@ import sqlite3
 from contextlib import closing
 
 if __name__ == '__main__':
-    with closing(sqlite3.connect('rc_logs.db')) as connection:
+    with closing(sqlite3.connect('rc_chat_log.db')) as connection:
         with closing(connection.cursor()) as cursor:
             print(cursor.execute("SELECT * FROM names").fetchall())
             print(cursor.execute("SELECT * FROM message_types").fetchall())

85a4d967c4a0c4c9ecde3562f408c19ddded0c36
Lee Pang <leepang15@gmail.com>
2023-02-17 02:10:16 -0600

    Revise for older SQLite version

--- 110_rename_columns.py
+++ 110_rename_columns.py
@@ -1,4 +1,3 @@
-import csv
 import sqlite3
 from contextlib import closing
 
@@ -7,16 +6,83 @@ if __name__ == '__main__':
     with closing(sqlite3.connect('rc_chat_log.db')) as connection:
         with closing (connection.cursor()) as cursor:
 
-            cursor.execute("    \
-                ALTER TABLE     \
-                    names RENAME TO usernames")
-            cursor.execute("    \
-                ALTER TABLE     \
-                    usernames RENAME COLUMN name_id to username_id")
-            cursor.execute("    \
-                ALTER TABLE     \
-                    usernames RENAME COLUMN name TO username")
+#            cursor.execute("    \
+#                ALTER TABLE     \
+#                    names RENAME TO usernames")
+#            cursor.execute("    \
+#                ALTER TABLE     \
+#                    usernames RENAME COLUMN name_id to username_id")
+#            cursor.execute("    \
+#                ALTER TABLE     \
+#                    usernames RENAME COLUMN name TO username")
 
-            cursor.execute("    \
-                ALTER TABLE     \
-                    messages RENAME COLUMN name_id to username_id")
+#            cursor.execute("    \
+#                ALTER TABLE     \
+#                    messages RENAME COLUMN name_id to username_id")
+            
+            cursor.execute("                            \
+                CREATE TABLE usernamesTemp (            \
+                    username_id INTEGER PRIMARY KEY,    \
+                    username TEXT NOT NULL,             \
+                    nickname TEXT)                      \
+                    ")
+
+            cursor.execute("                                                                    \
+                CREATE TABLE messagesTemp (                                                     \
+                    message_id INTEGER PRIMARY KEY,                                             \
+                    username_id INTEGER NOT NULL,                                               \
+                    message_type_id INTEGER NOT NULL,                                           \
+                    timestamp_ms INTEGER NOT NULL,                                              \
+                    item_count INTEGER NOT NULL,                                                \
+                    content TEXT,                                                               \
+                    FOREIGN KEY (username_id) REFERENCES usernamesTemp (username_id),           \
+                    FOREIGN KEY (message_type_id) REFERENCES message_types (message_type_id))   \
+                    ")
+            
+            cursor.execute("                                                \
+                INSERT INTO usernamesTemp (username_id, username, nickname) \
+                SELECT                                                      \
+                    name_id,                                                \
+                    name,                                                   \
+                    nickname                                                \
+                FROM                                                        \
+                    usernames                                               \
+                    ")
+            
+            cursor.execute("                                    \
+                INSERT INTO messagesTemp (                      \
+                    message_id, username_id, message_type_id,   \
+                    timestamp_ms, item_count, content           \
+                )                                               \
+                SELECT                                          \
+                    message_id,                                 \
+                    name_id,                                    \
+                    message_type_id,                            \
+                    timestamp_ms,                               \
+                    item_count,                                 \
+                    content                                     \
+                FROM                                            \
+                    messages                                    \
+                    ")
+            
+            cursor.execute("        \
+                DROP                \
+                    TABLE usernames \
+                    ")
+            
+            cursor.execute("        \
+                DROP                \
+                    TABLE messages  \
+                    ")
+            
+            cursor.execute("                            \
+                ALTER TABLE                             \
+                    usernamesTemp RENAME TO usernames   \
+                    ")
+            
+            cursor.execute("                            \
+                ALTER TABLE                             \
+                    messagesTemp RENAME TO messages     \
+                    ")
+            
+        connection.commit()

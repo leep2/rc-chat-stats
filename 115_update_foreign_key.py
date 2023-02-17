f84393f3578e8dbc3ee2e3240bdbbf1df6edb3d0
Lee Pang <leepang15@gmail.com>
2023-02-17 02:24:37 -0600

    Comment out already created table

--- 115_update_foreign_key.py
+++ 115_update_foreign_key.py
@@ -6,17 +6,17 @@ if __name__ == '__main__':
     with closing(sqlite3.connect('rc_chat_log.db')) as connection:
         with closing (connection.cursor()) as cursor:
 
-            cursor.execute("                                                                    \
-                CREATE TABLE messagesTemp (                                                     \
-                    message_id INTEGER PRIMARY KEY,                                             \
-                    username_id INTEGER NOT NULL,                                               \
-                    message_type_id INTEGER NOT NULL,                                           \
-                    timestamp_ms INTEGER NOT NULL,                                              \
-                    item_count INTEGER NOT NULL,                                                \
-                    content TEXT,                                                               \
-                    FOREIGN KEY (username_id) REFERENCES usernames (username_id),           \
-                    FOREIGN KEY (message_type_id) REFERENCES message_types (message_type_id))   \
-                    ")
+#            cursor.execute("                                                                    \
+#                CREATE TABLE messagesTemp (                                                     \
+#                    message_id INTEGER PRIMARY KEY,                                             \
+#                    username_id INTEGER NOT NULL,                                               \
+#                    message_type_id INTEGER NOT NULL,                                           \
+#                    timestamp_ms INTEGER NOT NULL,                                              \
+#                    item_count INTEGER NOT NULL,                                                \
+#                    content TEXT,                                                               \
+#                    FOREIGN KEY (username_id) REFERENCES usernames (username_id),           \
+#                    FOREIGN KEY (message_type_id) REFERENCES message_types (message_type_id))   \
+#                    ")
             
             cursor.execute("                                    \
                 INSERT INTO messagesTemp (                      \

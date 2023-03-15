ae774a16bdfc3ab7a5a000f889f50449897746cc
Lee Pang <leepang15@gmail.com>
2023-03-15 00:58:30 -0500

    Delete incorrect name of locked user

---
+++ delete_username.py
@@ -0,0 +1,12 @@
+import sqlite3
+from contextlib import closing
+
+if __name__ == '__main__':
+    with closing(sqlite3.connect('rc_chat_log.db')) as connection:
+        with closing (connection.cursor()) as cursor:
+            cursor.execute("                    \
+                DELETE FROM                     \
+                    usernames                   \
+                WHERE                           \
+                    username = 'Facebook user'  \
+                ")

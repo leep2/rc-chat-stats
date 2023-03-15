fe3c375a9c11b15070a2ffbac9c8897d6d9f2f66
Lee Pang <leepang15@gmail.com>
2023-03-15 00:14:57 -0500

    Add function for deleting last day from messages table

---
+++ delete_last_day.py
@@ -0,0 +1,26 @@
+import sqlite3
+from contextlib import closing
+from datetime import datetime
+
+if __name__ == '__main__':
+
+    with closing(sqlite3.connect('rc_chat_log.db')) as connection:
+        with closing (connection.cursor()) as cursor:
+
+            end_timestamp = cursor.execute("    \
+                SELECT                          \
+                    MAX(timestamp_ms)           \
+                FROM                            \
+                    messages                    \
+                ").fetchone()[0]
+            dt = datetime.fromtimestamp(end_timestamp/1000)
+            day = 1000 * datetime(dt.year, dt.month, dt.day).timestamp()            
+
+            cursor.execute("            \
+                DELETE FROM             \
+                    messages            \
+                WHERE                   \
+                    timestamp_ms >= ?   \
+                ", (day,)).fetchall()
+            
+        connection.commit()

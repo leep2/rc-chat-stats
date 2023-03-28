a6db59fda9e1401bab1951bfe04a5248126ea8f6
Lee Pang <leepang15@gmail.com>
2023-03-28 13:20:06 -0500

    Sort nickname list by nickname

--- view_nicknames.py
+++ view_nicknames.py
@@ -1,9 +1,14 @@
 import csv
 import sqlite3
 from contextlib import closing
-        
+
+def sort_key(record):
+    return record[1]
+
 with closing(sqlite3.connect('rc_chat_log.db')) as connection:
     with closing (connection.cursor()) as cursor:
         
         names = cursor.execute('SELECT username, nickname FROM usernames').fetchall()
-        print(names)
+        names.sort(key=sort_key)
+        for name in names:
+            print(name[1] + ', ' + name[0])
